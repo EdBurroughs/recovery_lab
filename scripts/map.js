4f0c7fa80950897a134639c6f7f6fb2a6b4237a2
Edward Burroughs <63171747+EdBurroughs@users.noreply.github.com>
2020-10-19 17:40:07 +0100

    another broken URL fixed and mapbox container fixed

--- scripts/map.js
+++ scripts/map.js
@@ -5,27 +5,23 @@ const coords = [-2.9663, 53.661];
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/ted1983/ckgfj187q3pu619pebhbfdkj8',        center: coords,
-    zoom: 15,
+    zoom: 16.5,
     center: coords
     //interactive: false,
+});
 
-    });
-
-let popup = new mapboxgl.Popup({
-        offset:[0,0]})
+const popup = new mapboxgl.Popup({
+        offset:[-200,-200]})
     .setText('popup text');
 
-let markerElement = document.createElement('div');
+const markerElement = document.createElement('div');
 markerElement.id = 'marker';
 
-let marker = new mapboxgl.Marker(markerElement)
+const marker = new mapboxgl.Marker(markerElement)
     .setLngLat(coords)
 
-const element = marker.getElement();
-element.id = 'marker';
-
-element.addEventListener('mouseenter', ()=> popup.addTo(map));
-element.addEventListener('mouseleave', ()=> popup.remove());
+marker.getElement().addEventListener('mouseenter', ()=> popup.addTo(map));
+marker.getElement().addEventListener('mouseleave', ()=> popup.remove());
 
 marker.setPopup(popup);
 marker.addTo(map);
